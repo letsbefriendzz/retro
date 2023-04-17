2a5c1b4956d673dd817f2c3f910d5311a67815ff
ryanenns <ryanennns@gmail.com>
2023-04-17 18:42:14 -0400

    add tailwind and daisyui

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vuePlugin from "@vitejs/plugin-vue";
+import { resolve } from 'path';
 
 export default defineConfig({
     plugins: [
@@ -11,4 +12,24 @@ export default defineConfig({
         vuePlugin()
     ],
     manifestDir: 'public/build',
+    resolve: {
+        alias: {
+            '@': resolve(__dirname, 'resources/js'),
+        },
+    },
+    css: {
+        postcss: {
+            plugins: [
+                require('tailwindcss'),
+                require('autoprefixer'),
+            ],
+        },
+    },
+    build: {
+        manifest: true,
+        outDir: 'public/build',
+        rollupOptions: {
+            input: '/resources/js/app.js',
+        },
+    },
 });
