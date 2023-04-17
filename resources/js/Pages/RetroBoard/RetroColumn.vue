f1d1c771dc93653e46ef6d4398c630ee0ba68c23
ryanenns <ryanennns@gmail.com>
2023-04-17 06:54:43 -0400

    inertia setup

---
+++ resources/js/Pages/RetroBoard/RetroColumn.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h1>{{this.columnOptions.title}}</h1>
+        <h3>{{this.columnOptions.description}}</h3>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "RetroColumn",
+    props: {
+        columnOptions: {
+            type: Object,
+            required: true,
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
